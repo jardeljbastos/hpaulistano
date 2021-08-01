ada2f447c5883d47f604415795092dfb62331d4b
realidademax <84300151+realidademax@users.noreply.github.com>
2021-07-31 21:35:50 -0300

    Initial commit

---
+++ data.js
@@ -0,0 +1,317 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-hpaulistano_salaexamei",
+      "name": "HPaulistano_SalaExameI",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.4470126831647434,
+        "pitch": 0.18563052556016935,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5640757799578573,
+          "pitch": 1.0205693846303916,
+          "rotation": 0.7853981633974483,
+          "target": "4-hpaulistano_salacomando"
+        },
+        {
+          "yaw": 2.891764558191113,
+          "pitch": 0.043360224706503914,
+          "rotation": 6.283185307179586,
+          "target": "5-hpaulistano_salatecnica"
+        },
+        {
+          "yaw": 2.910628917334658,
+          "pitch": 0.3295687680491497,
+          "rotation": 0,
+          "target": "3-hpaulistano_salaexameiv"
+        },
+        {
+          "yaw": -1.3685931382957666,
+          "pitch": 0.4873355162392059,
+          "rotation": 5.497787143782138,
+          "target": "1-hpaulistano_salaexameii"
+        },
+        {
+          "yaw": -2.3608661169268537,
+          "pitch": 0.10130755228332511,
+          "rotation": 3.141592653589793,
+          "target": "2-hpaulistano_salaexameiii"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-hpaulistano_salaexameii",
+      "name": "HPaulistano_SalaExameII",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.397247469540959,
+        "pitch": 0.20762675197748948,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.732439682430515,
+          "pitch": 0.35506215639674643,
+          "rotation": 3.141592653589793,
+          "target": "0-hpaulistano_salaexamei"
+        },
+        {
+          "yaw": 1.4816890645395286,
+          "pitch": 0.45917519768249804,
+          "rotation": 5.497787143782138,
+          "target": "4-hpaulistano_salacomando"
+        },
+        {
+          "yaw": -3.007017551711444,
+          "pitch": 0.4079171914113555,
+          "rotation": 3.141592653589793,
+          "target": "2-hpaulistano_salaexameiii"
+        },
+        {
+          "yaw": 2.166942488225703,
+          "pitch": 0.19046769788581663,
+          "rotation": 0.7853981633974483,
+          "target": "3-hpaulistano_salaexameiv"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-hpaulistano_salaexameiii",
+      "name": "HPaulistano_SalaExameIII",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.8876004045918133,
+        "pitch": 0.3002462375943402,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0281058951483946,
+          "pitch": 0.3406004951477488,
+          "rotation": 0.7853981633974483,
+          "target": "1-hpaulistano_salaexameii"
+        },
+        {
+          "yaw": -2.293540901052703,
+          "pitch": 0.41011668402358836,
+          "rotation": 0.7853981633974483,
+          "target": "5-hpaulistano_salatecnica"
+        },
+        {
+          "yaw": -2.5693848783900624,
+          "pitch": 0.3472345833162578,
+          "rotation": 3.141592653589793,
+          "target": "3-hpaulistano_salaexameiv"
+        },
+        {
+          "yaw": 2.6272333884214323,
+          "pitch": 0.09335947558002466,
+          "rotation": 8.63937979737193,
+          "target": "0-hpaulistano_salaexamei"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-hpaulistano_salaexameiv",
+      "name": "HPaulistano_SalaExameIV",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.469416365023232,
+        "pitch": 0.2449899252607608,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.0765460613236204,
+          "pitch": 1.0940789241240552,
+          "rotation": 11.780972450961727,
+          "target": "5-hpaulistano_salatecnica"
+        },
+        {
+          "yaw": -3.0370601002201187,
+          "pitch": 0.42185126476227275,
+          "rotation": 0,
+          "target": "0-hpaulistano_salaexamei"
+        },
+        {
+          "yaw": 1.542101259009602,
+          "pitch": 0.4127254995109162,
+          "rotation": 0.7853981633974483,
+          "target": "2-hpaulistano_salaexameiii"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-hpaulistano_salacomando",
+      "name": "HPaulistano_SalaComando",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 3.1337720175672263,
+        "pitch": 0.23796426848878482,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0993042425562596,
+          "pitch": 0.289806135924513,
+          "rotation": 0.7853981633974483,
+          "target": "0-hpaulistano_salaexamei"
+        },
+        {
+          "yaw": -3.05197748370235,
+          "pitch": 0.26125106200441905,
+          "rotation": 3.9269908169872414,
+          "target": "1-hpaulistano_salaexameii"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-hpaulistano_salatecnica",
+      "name": "HPaulistano_SalaTecnica",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.344223544030571,
+        "pitch": 0.21225642005617118,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4869466085963765,
+          "pitch": 0.9943855145430724,
+          "rotation": 0,
+          "target": "3-hpaulistano_salaexameiv"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
